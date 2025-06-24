9364983c69296bb9f0568a898c413ea1bd556435
Ernesto Caraballo <ernestojcaraballog@gmail.com>
2025-06-24 02:27:46 +0000

    Completed the domain name generatorç

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['Super', 'Mega', 'Mini', 'Minute', 'Medium', 'Normal'];
+let adjective = ['duper', 'mind', 'supper', 'detail', 'rare', 'time'];
+let noun = ['break', 'crumble', 'wipeout', 'slash', 'null', 'complete'];
+let description = ['game', 'network', 'surfing', 'sports', 'tube', 'crisis'];
+let domain = ['.com', '.io', '.en', '.es', '.gov', '.info'];
 
+const combinations = () =>{
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    for (let index = 0; index < pronoun.length; index++) {
+        const elements = pronoun[index];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+        for (let indexAdjective = 0; indexAdjective < adjective.length; indexAdjective++) {
+            const eleAdjective = adjective[indexAdjective];
+
+            for (let indexNoun = 0; indexNoun < noun.length; indexNoun++) {
+                const nounNoun = noun[indexNoun];
+
+                for (let indexDomain = 0; indexDomain < domain.length; indexDomain++){
+                    const domDomain = domain[indexDomain];
+
+                    let result = `${elements}${eleAdjective}${nounNoun}${domDomain}`;
+                    console.log(result);
+                } 
+            }
+        }
+
+    }
+
+}
+
+combinations() 
